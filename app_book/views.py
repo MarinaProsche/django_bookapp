b8da4138a514f48c7126f3c1f0c56e45f3106b9f
Marina <verika_iz_virgo@mail.ru>
2025-03-11 13:31:08 +0300

    links to next_page added

--- app_book/views.py
+++ app_book/views.py
@@ -9,8 +9,13 @@ def heads(request):
 def chapter(request, pk):
     chapter_text = get_object_or_404(Text, pk=pk)
     chapter_buzzwords = BuzzWord.objects.filter(text=chapter_text)
-    return render(request, 'single_chapter.html', {'text': chapter_text, 'chapter_buzzwords': chapter_buzzwords})
+    try:
+        next_chapter=Text.objects.get(pk=(pk+1)).pk
+    except Text.DoesNotExist:
+        next_chapter = None
+    return render(request, 'single_chapter.html', {'text': chapter_text,
+                                                   'chapter_buzzwords': chapter_buzzwords,
+                                                   'next_chapter': next_chapter})
 
 def greeting(request):
-    
     return render(request, 'greetings.html')
