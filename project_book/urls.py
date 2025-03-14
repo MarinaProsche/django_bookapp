df8e79b664e588bd57ef49abf42889bf9fb5ae68
Marina <verika_iz_virgo@mail.ru>
2025-03-14 19:17:08 +0300

    +favorites

--- project_book/urls.py
+++ project_book/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path('chapters/', views.heads, name='heads'),
     path('chapters/<int:pk>/', views.chapter, name='chapter'),
     path('', views.greeting, name='greeting'),
-    path('postcards', views.postcards, name='postcards')
+    path('postcards', views.postcards, name='postcards'),
+    path('add_to_favorite/<int:id>', views.add_to_favorite, name='add_to_favorites'),
+    path('favorites/', views.favorites, name='favorites')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
