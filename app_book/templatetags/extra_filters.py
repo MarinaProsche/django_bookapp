e0455473031bdd1de51bb7cc74997403fc19f149
Marina <verika_iz_virgo@mail.ru>
2025-03-31 17:04:08 +0300

    set modal images

--- app_book/templatetags/extra_filters.py
+++ app_book/templatetags/extra_filters.py
@@ -6,25 +6,20 @@ register = template.Library()
 # @register.filter(name='replace')
 # def replace(text, buzzwords):
 #     for buzzword in buzzwords:
-#         # if buzzword.buzzword in text.lower():
-#         text = re.sub(
-#             rf'(?i)\b{re.escape(buzzword.buzzword)}\b',
-#             f"<a href='{buzzword.linked_file.link_to_image}'>{buzzword.buzzword}</a>",
-#             text
-#         )
-#     return text
+#         pattern = rf'(?i)(?<!\w){re.escape(buzzword.buzzword)}(?!\w|\.)'
+#         def replace_case(match):
+#             matched_text = match.group(0)
+#             return f"<a href='{buzzword.linked_file.link_to_image}' class='text-lavender' style='color: #9654c5'>{matched_text}</a>"
+#         text = re.sub(pattern, replace_case, text)
 
+#     return text
 
 @register.filter(name='replace')
 def replace(text, buzzwords):
     for buzzword in buzzwords:
         pattern = rf'(?i)(?<!\w){re.escape(buzzword.buzzword)}(?!\w|\.)'
-        # pattern = rf'(?i)(?<!\w){re.escape(buzzword.buzzword)}(?!\w)'
-        # pattern = rf'(?i)\b{re.escape(buzzword.buzzword)}\b' #case-insensitive, includes spec symbols just in case
         def replace_case(match):
             matched_text = match.group(0)
-            output_ex = f"<a href='{buzzword.linked_file.link_to_image}'>{matched_text}</a>"
-            return output_ex
+            return f"<span class='buzzword' onclick=\"openModal('{buzzword.linked_file.file}', event)\">{matched_text}</span>"
         text = re.sub(pattern, replace_case, text)
-    
     return text
