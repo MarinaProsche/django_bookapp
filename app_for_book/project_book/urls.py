bc3dbbd556d413f387a2fd0047a2c526c02434f3
Marina <verika_iz_virgo@mail.ru>
2025-07-23 18:37:45 +0300

    removed cache from worker

--- app_for_book/project_book/urls.py
+++ app_for_book/project_book/urls.py
@@ -1,23 +1,29 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
 from app_book import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.decorators.cache import cache_page
+from django.views.generic import TemplateView
 from pwa import views as pwa_views
 
 urlpatterns = [
+    # path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('admin/', admin.site.urls),
     path('chapters/', views.heads, name='heads'),
     path('chapters/<int:pk>/', views.chapter, name='chapter'),
-    path('greetings', views.greeting, name='greeting'),
+    path('', views.greeting, name='greeting'),
     path('postcards', views.postcards, name='postcards'),
     path('add_to_favorite/<int:pic_id>/', views.add_to_favorite, name='add_to_favorites'),
     path('favorites/', views.favorites, name='favorites'),
     path('logout/', views.logout_view, name='logout'),
     path('map/', views.map, name='map'),
-    path('', include('pwa.urls')),
     path('postcard/<int:target_buzzword>/', views.single_postcard, name='single_postcard'),
-    path('registration/', views.registration, name='registration')
-    # path('offline/', cache_page(settings.PWA_APP_NAME)(pwa_views.offline.as_view())),
+    path('registration/', views.registration, name='registration'),
+    path('', include('pwa.urls')),
+
+
+    re_path(r'^manifest\.json$', pwa_views.manifest, name='manifest'),
+    re_path(r'^serviceworker\.js$', pwa_views.service_worker, name='serviceworker'),
+    path('offline/', pwa_views.offline, name='offline'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app_for_book/staticfiles/js/serviceworker.js
+++ app_for_book/staticfiles/js/serviceworker.js
@@ -1,27 +1,11 @@
-
-var staticCacheName = 'djangopwa-v1';
-
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(staticCacheName).then(function(cache) {
-      return cache.addAll([
-        '',
-      ]);
-    })
-  );
+  console.log('Service Worker created');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', function(event) {
-  var requestUrl = new URL(event.request.url);
-    if (requestUrl.origin === location.origin) {
-      if ((requestUrl.pathname === '/')) {
-        event.respondWith(caches.match(''));
-        return;
-      }
-    }
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
 });
